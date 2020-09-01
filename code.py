ff57b2f82f332f7d098a0ab54c36f28adc42ca7c
Azmain946 <70563213+Azmain946@users.noreply.github.com>
2020-09-01 17:55:27 +0600

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 message = MIMEMultipart("alternative")
 
-
+n=int(input()) #how much email do you want to send
 report_file = open('') #locate the html code here
 html = report_file.read()
 
@@ -24,6 +24,6 @@ message.attach(part2)
 s = smtplib.SMTP(host='smtp.gmail.com', port=587)
 s.starttls()
 s.login(Your email, Your Password)
-for i in range(1):
+for i in range(n):
 	s.send_message((message))         
 	print("Successfully sent email")
